97e04798aeda3ab435f45204e43cbbdf4b13685d
spacemeowx2 <spacemeowx2@gmail.com>
2020-06-02 04:48:01 +0800

    add 98.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
+import '98.css'
 
 const Index: React.FC = () => {
+  const [ count, setCount ] = React.useState(0)
+
   return <>
-    <p>Hello world</p>
+    <div style={{ width: 300 }} className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">Counter</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize" />
+          <button aria-label="Maximize" />
+          <button aria-label="Close" />
+        </div>
+      </div>
+
+      <div className="window-body">
+        <p style={{ textAlign: "center" }}>Current count: {count}</p>
+        <div className="field-row" style={{ justifyContent: "center" }}>
+          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={() => setCount(count - 1)}>-</button>
+          <button onClick={() => setCount(0)}>0</button>
+        </div>
+      </div>
+    </div>
   </>
 }
 
